d4123180c68f37b0ded9539f3ec7f759599a47e2
JIHUN HONG <hjihun2315@likelion.org>
2018-11-08 18:22:46 +0000

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users
 
-  get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
