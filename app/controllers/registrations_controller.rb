4aa7bb13a19ebe83bc5f17e3244e728a85e54565
khb1109 <fusis1@naver.com>
2018-11-09 20:44:08 +0900

    유저 뷰 수정, 인덱스 페이지 수

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,24 @@
+# 회원가입 폼을 오버라이드하기위한 컨트롤러
+class RegistrationsController < Devise::RegistrationsController
+
+    def new
+        super
+    end
+
+    def create
+        
+        @user = User.new(params.require(:user).permit(:name, :phone, :email, :password))
+        @user.save
+        if params[:name] != "" 
+            @farm = Farm.new(params.permit(:name, :farm_type, :admin, :location))
+            @farm.user_id = User.last.id
+            @farm.save
+        end        
+        
+        redirect_to root_url
+    end
+
+    def update
+        super
+    end
+end 
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20181109080052) do
-=======
 ActiveRecord::Schema.define(version: 20181109080032) do
->>>>>>> origin/user_authorization
 
   create_table "farms", force: :cascade do |t|
     t.string   "name",       default: "", null: false
