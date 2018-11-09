2e2b45eff39a107878a05452012cbe1780c6d7f9
Hong seoyeoung <yy7062@likelion.org>
2018-11-09 12:09:31 +0000

    게시판 글쓰기

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20181109080052) do
-=======
-ActiveRecord::Schema.define(version: 20181109080032) do
->>>>>>> origin/user_authorization
 
   create_table "farms", force: :cascade do |t|
-    t.string   "name",       default: "", null: false
-    t.string   "farm_type",  default: "", null: false
-    t.string   "admin",      default: "", null: false
-    t.string   "location",   default: "", null: false
+    t.string   "name"
+    t.string   "farm_type"
+    t.string   "admin"
     t.integer  "user_id"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_farms_on_user_id"
   end
 
@@ -42,6 +37,17 @@ ActiveRecord::Schema.define(version: 20181109080032) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.string   "resource_type"
+    t.integer  "resource_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["name"], name: "index_roles_on_name"
+    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
+  end
+
   create_table "serves", force: :cascade do |t|
     t.string   "title"
     t.string   "content"
@@ -54,16 +60,6 @@ ActiveRecord::Schema.define(version: 20181109080032) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_serves_on_user_id"
-    
-  create_table "roles", force: :cascade do |t|
-    t.string   "name"
-    t.string   "resource_type"
-    t.integer  "resource_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
-    t.index ["name"], name: "index_roles_on_name"
-    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
   end
 
   create_table "users", force: :cascade do |t|
